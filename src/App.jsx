98efe7d8719392ef33195f58bf218e4d12a9a7b3
Lemmy Mwaura <lemminton@gmail.com>
2022-06-10 18:35:41 +0300

    feat(reactQuery): setup react queryClientProvider and reacrQuery devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import Header from './components/Header';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Query</h1>
-      <Header/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>React Query</h1>
+        <Header/>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom right'/>
+    </QueryClientProvider>
   )
 }
 
