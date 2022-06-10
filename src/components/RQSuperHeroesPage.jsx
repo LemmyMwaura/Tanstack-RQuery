5e5dc92eac24b2fea45a2e96e8e6caea1b87af73
Lemmy Mwaura <lemminton@gmail.com>
2022-06-10 16:33:45 +0300

    feat: data fetching using react query

--- src/components/RQSuperHeroesPage.jsx
+++ src/components/RQSuperHeroesPage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useQuery } from "react-query"
+import axios from "axios"
 
 export default function RQSuperHeroespage() {
+  const { isLoading, isError, error, data } = useQuery("super-heroes", () => {
+    return axios
+      .get("http://localhost:4000/superheroes")
+  })
+  
   return (
-    <div></div>
+    <div>
+      <h2 className="title">RQ Super Heroes Page</h2>
+
+      {isError && (
+        <div>{error.message}</div>
+      )}
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        data?.data.map((hero) => {
+          return <div key={hero.id}>{hero.name}</div>
+        })
+      )}
+    </div>
   )
 }
