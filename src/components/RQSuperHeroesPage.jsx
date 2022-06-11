c91ada1863351a2f9b3bfd28bb46723f3b4c81a7
Lemmy Mwaura <lemminton@gmail.com>
2022-06-11 13:41:58 +0300

    feat: move the data fetching fucntionality to a custom react query hook

--- src/components/RQSuperHeroesPage.jsx
+++ src/components/RQSuperHeroesPage.jsx
@@ -1,51 +1,33 @@
-import { useQuery } from "react-query"
-import axios from "axios"
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData"
 import { useState } from "react"
 
 export default function RQSuperHeroespage() {
   const [interval, setInterval] = useState(3000)
   const onSuccess = ({ data }) => {
-    console.log('Perform side effect after data fetching', data)
-    if(data?.length >= 4){
+    console.log("Perform side effect after data fetching", data)
+    if (data?.length >= 4) {
       setInterval(0)
     }
     setInterval(0)
   }
 
   const onError = (error) => {
-    console.log('Perform side effect after encoutering error', error)
+    console.log("Perform side effect after encoutering error", error)
     setInterval(0)
   }
 
-  const { isLoading, isError, error, data } = useQuery("super-heroes", () => {
-    return axios
-      .get("http://localhost:4000/superheroes")
-  },
-  {
-    // cacheTime: 5000000,
-    // staleTime:0,
-    // refetchOnMount: true,
-    // refetchOnWindowFocus: true,
-    refetchInterval:interval, //NB stops when browser losses focus
-    // refetchIntervalInBackground: true, //When sets to true, Keeps fetching data even when the browser loses focus
-    // enabled:false //Fetch on user request e.g click of a button
+  const { isLoading, isError, error, data } = useSuperHeroesData(
     onSuccess,
     onError,
-    select: (data) => {
-      //Select performs data transformation and also data filtering
-      const superheroesNames = data.data.map(hero => hero.name)
-      return superheroesNames
-    }
-  })
-  
+    interval
+  )
+
   return (
     <div>
       <h2 className="title">RQ Super Heroes Page</h2>
       {/* <button onClick={refetch}>Fetch Heroes</button> */}
 
-      {isError && (
-        <div>{error.message}</div>
-      )}
+      {isError && <div>{error.message}</div>}
 
       {isLoading ? (
         <div>Loading...</div>
---
+++ src/hooks/useSuperHeroesData.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+
+const fetchUserData = () => {
+  return axios.get("http://localhost:4000/superheroes")
+}
+
+export const useSuperHeroesData = (onSuccess, onError, interval) => {
+  return useQuery("super-heroes", fetchUserData, {
+    // cacheTime: 5000000,
+    // staleTime:0,
+    // refetchOnMount: true,
+    // refetchOnWindowFocus: true,
+    refetchInterval: interval, //NB stops when browser losses focus
+    // refetchIntervalInBackground: true, //When sets to true, Keeps fetching data even when the browser loses focus
+    // enabled:false //Fetch on user request e.g click of a button
+    onSuccess,
+    onError,
+    select: (data) => {
+      //Select performs data transformation and also data filtering
+      const superheroesNames = data.data.map((hero) => hero.name)
+      return superheroesNames
+    },
+  })
+}
