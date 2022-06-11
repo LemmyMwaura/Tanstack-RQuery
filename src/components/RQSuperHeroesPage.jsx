e7e8caa7ec0aff062a59e3bb282879557dc65a09
Lemmy Mwaura <lemminton@gmail.com>
2022-06-11 13:24:45 +0300

    feat: implement data transformation

--- src/components/RQSuperHeroesPage.jsx
+++ src/components/RQSuperHeroesPage.jsx
@@ -4,12 +4,12 @@ import { useState } from "react"
 
 export default function RQSuperHeroespage() {
   const [interval, setInterval] = useState(3000)
-
   const onSuccess = ({ data }) => {
     console.log('Perform side effect after data fetching', data)
-    if(data.length >= 4){
+    if(data?.length >= 4){
       setInterval(0)
     }
+    setInterval(0)
   }
 
   const onError = (error) => {
@@ -30,7 +30,12 @@ export default function RQSuperHeroespage() {
     // refetchIntervalInBackground: true, //When sets to true, Keeps fetching data even when the browser loses focus
     // enabled:false //Fetch on user request e.g click of a button
     onSuccess,
-    onError
+    onError,
+    select: (data) => {
+      //Select performs data transformation and also data filtering
+      const superheroesNames = data.data.map(hero => hero.name)
+      return superheroesNames
+    }
   })
   
   return (
@@ -45,8 +50,11 @@ export default function RQSuperHeroespage() {
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        data?.data.map((hero) => {
-          return <div key={hero.id}>{hero.name}</div>
+        // data?.data.map((hero) => {
+        //   return <div key={hero.id}>{hero.name}</div>
+        // })
+        data.map((heroName) => {
+          return <div key={heroName}>{heroName}</div>
         })
       )}
     </div>
